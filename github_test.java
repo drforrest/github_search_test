a02eef9812552234fec9b86ce5b1fbb0a04a2924
drforrest <40846986+drforrest@users.noreply.github.com>
2025-07-04 11:04:02 +0800

    Update github_test.java

--- github_test.java
+++ github_test.java
@@ -1,5 +1,10 @@
 public class github_test {
     public static void main(String[] args) {
         System.out.println("nosecret.cht.com.tw");
+        System.out.println("nosecret.chttl.com.tw");
+        System.out.println("tw.cht.com");
+        System.out.println("tw.chttl.com");
+        System.out.println("chttl");
+        System.out.println("cht");
     }
 }
